d45063f1c3d33e5a8a83d076533affd15897002c
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-06-17 16:18:17 +0100

    prepare for heroku

--- data_justice/wsgi.py
+++ data_justice/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "data_justice.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
