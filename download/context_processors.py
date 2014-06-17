9793f4ca68ba86395b2defbb32702cf2677ba192
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-06-17 15:56:04 +0100

    more text and better layout

--- download/context_processors.py
+++ download/context_processors.py
@@ -1,7 +1,7 @@
 def globals(request):
   return {
-    'app_title': 'Download data', # Application Title (Populates <title>)
-    'proposition_title': 'Download Data', # Proposition Title (Populates proposition header)
+    'app_title': 'MOJ Open Data Platform', # Application Title (Populates <title>)
+    'proposition_title': 'MOJ Open Data Platform', # Proposition Title (Populates proposition header)
     'phase': 'alpha', # Current Phase (Sets the current phase and the colour of phase tags). Presumed values: alpha, beta, live
     'product_type': 'information', # Product Type (Adds class to body based on service type). Presumed values: information, service
     'feedback_url': '', # Feedback URL (URL for feedback link in phase banner)
