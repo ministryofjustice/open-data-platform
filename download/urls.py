ef95f3a9d444191b4ea9c1056f4290c1994e4cf7
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-06-19 10:57:09 +0100

    documentation page

--- download/urls.py
+++ download/urls.py
@@ -4,5 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='download/index.html'), name='index'),
-    url(r'^go/$', views.GoView.as_view(), name='go')                       
+    url(r'^doc/$', TemplateView.as_view(template_name='download/doc.html'), name='doc'),
+    url(r'^go/$', views.GoView.as_view(), name='go'),
+
 )
