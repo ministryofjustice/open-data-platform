86f882a9a5560fb080ead1785298a1bcf9981d60
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-06-24 12:35:08 +0100

    single page for applications for now

--- apps/urls.py
+++ apps/urls.py
@@ -3,5 +3,4 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='apps/index.html'), name='index'),
-    url(r'^demo1$', TemplateView.as_view(template_name='apps/demo1.html'), name='demo1')
 )
